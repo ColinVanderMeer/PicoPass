fbbe2b36e67a8846830359ae113ecab8223cc0de
ColinVanderMeer <colinvandermeer@gmail.com>
2024-07-06 14:32:31 -0400

    Broken version that kinda works (no scan)

--- main.py
+++ main.py
@@ -6,32 +6,54 @@ import machine
 
 import asyncio
     
-async def startAP(ssid, password):
-    print("start")
-    ap = network.WLAN(network.AP_IF)
-    ap.config(essid=ssid, password=password)
-    ap.active(True)
-    
+async def startAP():
     while not ap.active():
         await asyncio.sleep(1)  # Sleep asynchronously
 
     print('AP Mode Is Active, You can Now Connect')
     print('IP Address To Connect to:: ' + ap.ifconfig()[0])
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket object
+    s.bind(('', 80))
+    s.listen(5)
+
+    while True:
+      conn, addr = s.accept()
+      print('Got a connection from %s' % str(addr))
+      request = conn.recv(1024)
+      print('Content = %s' % str(request))
+      response = "TestResponse"
+      conn.send(response)
+      conn.close()
 
 async def scanNetwork():
+    print("test3")
     # Connect to WLAN
     networks = wlan.scan()
     print(networks)
+    for listItem in networks:
+        print(listItem)
 
 async def main():
 
-    startAP_task = asyncio.create_task(startAP("test123", "toor"))
-
+    startAP_task = asyncio.create_task(startAP())
+    
+    print("we got here")
+    
     while True:
+        print("we here tooo :3")
+        print("test")
         scanForPico_task = asyncio.create_task(scanNetwork())
-        await asyncio.sleep_ms(5_000)
+        print("here?")
+        await asyncio.sleep(1)
         
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
+
+ap = network.WLAN(network.AP_IF)
+ap.config(essid="PicoPass678", password="PicoPassword")
+ap.active(True)
+
+wlan.disconnect()
 
 asyncio.run(main())
--- testFile.py
+++ testFile.py
@@ -1,77 +1,49 @@
 import network
-import socket
 import time
-from picozero import pico_temp_sensor, pico_led
-import machine
+import socket
 
-import asyncio
 
-async def scanNetwork():
-    print("21")
-    # Connect to WLAN
-    wlan = network.WLAN(network.STA_IF)
-    print("22")
-    wlan.active(True)
-    print("23")
-    networks = wlan.scan()
-    print("24")
-    print(networks)
-    print("25")
-    
-async def web_page():
-    html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
-              <body><h1>Hello World</h1></body></html>
-           """
-    return html
+def web_page():
+  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+            <body><h1>Hello World</h1></body></html>
+         """
+  return html
 
-async def ap_mode(ssid, password):
+# if you do not see the network you may have to power cycle
+# unplug your pico w for 10 seconds and plug it in again
+def ap_mode(ssid, password):
     """
         Description: This is a function to activate AP mode
-        
+
         Parameters:
-        
+
         ssid[str]: The name of your internet connection
         password[str]: Password for your internet connection
-        
+
         Returns: Nada
     """
     # Just making our internet connection
     ap = network.WLAN(network.AP_IF)
     ap.config(essid=ssid, password=password)
     ap.active(True)
-    
-    while not ap.active():
-        await asyncio.sleep(1)  # Sleep asynchronously
 
+    while ap.active() == False:
+        pass
     print('AP Mode Is Active, You can Now Connect')
     print('IP Address To Connect to:: ' + ap.ifconfig()[0])
-    
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # creating socket object
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket object
     s.bind(('', 80))
     s.listen(5)
-    s.setblocking(False)  # Set socket to non-blocking mode
-
-    loop = asyncio.get_event_loop()
 
     while True:
-        try:
-            conn, addr = await loop(s.accept())
-            print('Got a connection from %s' % str(addr))
-            request = await loop.sock_recv(conn, 1024)
-            print('Content = %s' % str(request))
-            response = await web_page()
-            await loop.sock_sendall(conn, response.encode())  # Ensure the response is in bytes
-            conn.close()
-        except OSError as e:
-            if e.errno != 11:  # EAGAIN
-                raise
-            await asyncio.sleep(1)
+      conn, addr = s.accept()
+      print('Got a connection from %s' % str(addr))
+      request = conn.recv(1024)
+      print('Content = %s' % str(request))
+      response = web_page()
+      conn.send(response)
+      conn.close()
 
-async def main():
-    ap_task = asyncio.create_task(ap_mode('NAME', 'PASSWORD'))
-    await asyncio.sleep_ms(5_000)
-    scan_task = asyncio.create_task(scanNetwork())
-
-    await asyncio.sleep_ms(10_000)
-
-asyncio.run(main())
+ap_mode('NAME',
+        'PASSWORD')
