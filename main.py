6a80a2beec0283d630761fa05b4c110d710873b7
ColinVanderMeer <colinvandermeer@gmail.com>
2024-07-21 13:34:06 -0400

    Detect other PicoPass (currently doesn't recieve data)

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+# PicoPass main
+# AI was used to help debug a few async errors (Claude 3.5 Sonnet)
 import network
 import socket
 import time
 from picozero import pico_temp_sensor, pico_led
 import machine
 import asyncio
+import urequests
+
+networkList = []
 
 async def handle_client(conn, addr):
     print('Got a connection from %s' % str(addr))
@@ -56,22 +61,60 @@ async def startAP():
         await asyncio.sleep(0.1)
 
 async def scanNetwork():
+    networkList.clear()
     networks = wlan.scan()
-    print(networks)
-    for listItem in networks:
-        print(listItem)
+    for tup in networks:
+        ssidName = tup[0].decode("utf-8")
+        if ssidName != "":
+            networkList.append(ssidName)
+            
+async def search_picopass(items_list):
+    return [item for item in items_list if "PicoPass" in item]
+
+async def getData(ssid):
+    attempts = 0
+    #Connect to WLAN
+    wlan.connect(ssid, "PicoPassword")
+    while not wlan.isconnected():
+        print('Waiting for connection...')
+        await asyncio.sleep(1)
+        attempts += 1
+        if attempts == 10:
+            return "ERROR: Connection timeout"
+    ip = wlan.ifconfig()[0]
+    print(f'Connected on {ip}')
+    
+    try:
+        response = urequests.get("http://192.168.4.1")
+        print("Response received")
+        print(response.text)
+        response.close()
+        return response.text
+    except Exception as e:
+        print(f"Error during request: {e}")
+        return f"ERROR: {str(e)}"
+    finally:
+        pico_led.off()
+        wlan.disconnect()
 
 async def main():
     startAP_task = asyncio.create_task(startAP())
     
     while True:
         await scanNetwork()
+        print(networkList)
+        picoPassList = await search_picopass(networkList)
+        
+        if picoPassList:
+            print("PicoPass Found")
+            pico_led.on()
+            print(await getData(picoPassList[0]))
         await asyncio.sleep(10)  # Sleep for 5 seconds before scanning again
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 ap = network.WLAN(network.AP_IF)
-ap.config(essid="PicoPass678", password="PicoPassword")
+ap.config(essid="PicoPass123", password="PicoPassword")
 ap.active(True)
 wlan.disconnect()
 
